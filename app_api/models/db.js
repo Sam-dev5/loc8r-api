888bb520d129c81c7e63d780d514a95aa19f0d89
Sam-dev5 <samkeun5@gmail.com>
2024-10-02 07:16:31 +0900

    Add files via upload

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -4,8 +4,7 @@ mongoose.set("strictQuery", false);
 
 // const dbURI = 'mongodb://localhost/Loc8r';
 const dbPassword = process.env.MONGODB_PASSWORD;
-// const dbURI = `mongodb+srv://my_atlas_user:${dbPassword}@cluster0.s0fko.mongodb.net/Loc8r`;
-const dbURI = `mongodb+srv://my_atlas_user:tkarms77@cluster0.s0fko.mongodb.net/Loc8r`;
+const dbURI = `mongodb+srv://my_atlas_user:${dbPassword}@cluster0.s0fko.mongodb.net/Loc8r`;
 
 const connect = () => {
   setTimeout(() => mongoose.connect(dbURI), 1000);
